b299e7b5d5bad25d1d5733bc9de2057af91febbb
SageSphinx63920 <sage.sphinx63920@gmail.com>
2021-05-22 00:41:17 +0200

    Deleted token class

--- src/de/sage/main/DONOTOPEN.java
+++
@@ -1,8 +0,0 @@
-package de.sage.main;
-
-public class DONOTOPEN {
-	
-	public static String Token = "ODM4MDYyNTc0OTYzNTIzNjQ0.YI1omA.tAqF0YR5IjlEKSuaO6sTaRzuVPs";
-	public static String DevToken = "";
-
-}
