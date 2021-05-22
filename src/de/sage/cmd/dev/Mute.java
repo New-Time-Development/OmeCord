251cceb4b46b36c9e653e2b7cf8d04208a40e2e6
Sage <sage.sphinx63920@gmail.com>
2021-05-23 01:36:41 +0200

    Updated all files

--- src/de/sage/cmd/dev/Mute.java
+++
@@ -1,5 +0,0 @@
-package de.sage.cmd.dev;
-
-public class Mute {
-
-}
--- src/de/sage/system/OmeUser.java
+++
@@ -1,9 +0,0 @@
-package de.sage.system;
-
-public interface OmeUser {
-	
-	public String getGuild();
-	public void setUserid(Long userid);
-	public void setGuildid(Long guildid);
-
-}
