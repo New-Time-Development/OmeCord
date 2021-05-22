346c3771fa4817dd96591d26eb8fce4b2fcd25e2
Sage <finn.pillong@web.de>
2021-05-23 01:37:43 +0200

    Upload new Utils

---
+++ util/RandomInt.java
@@ -0,0 +1,16 @@
+package de.sage.util;
+
+import java.util.Random;
+
+public class RandomInt {
+
+
+	public static int randInt(int min, int max) {
+
+	    Random rand = new Random();
+
+	    int randomNum = rand.nextInt((max - min) + 1) + min;
+
+	    return randomNum;
+	}
+}
