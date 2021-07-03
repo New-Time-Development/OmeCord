34eacbff8d04f9ef86c1940840335d9a1913c554
Sage <finn.pillong@web.de>
2021-07-03 23:26:29 +0200

    Added vote command

--- src/com/newtime/command/slash/VoteSlashCommand.java
+++ src/com/newtime/command/slash/VoteSlashCommand.java
@@ -1,26 +1,20 @@
-package com.newtime.command;
+package com.newtime.command.slash;
 
 import java.awt.Color;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
-import java.util.Random;
 
 import com.newtime.database.LiteSQL;
 import com.newtime.main.Main;
-import com.newtime.util.Error;
 import com.newtime.util.TopGG;
 import com.newtime.util.Translations;
 
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
-import net.dv8tion.jda.api.entities.Message;
-import net.dv8tion.jda.api.entities.TextChannel;
-import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.interaction.SlashCommandEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
 /**
@@ -29,59 +23,34 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
  * Copyright (c) 2019 - 2021 by New Time Development/Sage to present. All rights reserved
  */
 
-
-public class VoteCommand extends ListenerAdapter{
+public class VoteSlashCommand extends ListenerAdapter{
 	
 	@SuppressWarnings("unused")
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		  if (event.getMessage().getContentRaw().startsWith(Main.prefix + "vote")) {
-	        	//Angaben
-	        	TextChannel ch = event.getChannel();
-	        	Member m = event.getMember();
-	        	Guild g = event.getGuild();
-	        	Message mes = event.getMessage();
-	        	List<Color> ColorList = Arrays.asList(Color.GREEN,Color.BLUE, Color.CYAN, Color.LIGHT_GRAY, Color.MAGENTA, Color.ORANGE, Color.PINK, Color.RED, Color.WHITE, Color.YELLOW, Color.decode("#0b0064"));
-				Color Color_RANDOM = ColorList.get(new Random().nextInt(ColorList.size()));
-				String[] args = event.getMessage().getContentDisplay().split(" ");
-				int length = args.length;
-				 StringBuilder strbuild = new StringBuilder();
-																									     //1 	2
-				for(int i = 2; i < length; i++) strbuild.append(args[i] + " "); //i = anzahl von suvfix: e.ichbincool
-					
-				String argsstring = strbuild.toString().trim();
-				
-				ResultSet lanRS = LiteSQL.onQuery("SELECT * FROM lang WHERE userid = " + m.getIdLong());
-				String lan = null;
-				try {
-					if(lanRS.next()) {
-						lan = lanRS.getString("botLan");
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+	public void onSlashCommand(SlashCommandEvent event) {
+		
+		if(event.getName().equalsIgnoreCase("vote")) {
+			Guild g = event.getGuild();
+			Member m = event.getMember();
+			
+			ResultSet lanRS = LiteSQL.onQuery("SELECT * FROM lang WHERE userid = " + m.getIdLong());
+			String lan = null;
+			try {
+				if(lanRS.next()) {
+					lan = lanRS.getString("botLan");
 				}
-				ResultSet premiumRS = LiteSQL.onQuery("SELECT * FROM premium WHERE userid = " + m.getIdLong());
-				boolean premium = false;
-				try {
-					if(premiumRS.next()) {
-						premium = true;
-					}
-				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				
-				
-				//Hier gehts los
-				if(length == 1) {
-					//Hier gehts los
-		          	  ch.sendMessageEmbeds(new EmbedBuilder()
-		          			  .setColor(Color_RANDOM)
-		          			  .setTitle("Vote for me!")
-		          			  .setDescription(Translations.Vote(lan, "info", m))
-		          			  .build()).queue();      
-				}else if(args[1].toLowerCase().equalsIgnoreCase("check")) {
-					
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			if(event.getOptions().size() == 0) {
+				 event.replyEmbeds(new EmbedBuilder()
+	          			  .setColor(Color.YELLOW)
+	          			  .setTitle("Vote for me!")
+	          			  .setDescription(Translations.Vote(lan, "info", m))
+	          			  .build()).queue();     
+			}else {
+				if(event.getOption("command").getAsString().equalsIgnoreCase("check")){
 					ResultSet set = LiteSQL.onQuery("SELECT * from vote WHERE userid = " + m.getIdLong());
 					try {
 						if(!set.next()) {
@@ -127,24 +96,24 @@ public class VoteCommand extends ListenerAdapter{
 												LiteSQL.onUpdate("UPDATE vote SET time = " + timer + " WHERE userid = " + m.getIdLong());
 												LiteSQL.onUpdate("UPDATE vote SET coins = " + coinsN + " WHERE userid = " + m.getIdLong());
 												
-												ch.sendMessage("Your vote was accepted. Check <#856892710367002665> out!").queue();
+												event.reply("Your vote was accepted. Check <#856892710367002665> out!").queue();
 												} catch (SQLException e1) {
 													// TODO Auto-generated catch block
 													e1.printStackTrace();
 												}
 											}else
-												ch.sendMessage("You haven´t voted!").queue();
+												event.reply("You haven´t voted!").queue();
 							  
 
 								});
 							}else
-								ch.sendMessage("You have already checked your vote!").queue();
+								event.reply("You have already checked your vote!").queue();
 						
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-				}else if(args[1].equalsIgnoreCase("list")) {
+				}else if(event.getOption("command").getAsString().equalsIgnoreCase("list")){
 					ResultSet set = LiteSQL.onQuery("SELECT * from vote WHERE userid = " + m.getIdLong());
 					try {
 						if(!set.next()) {
@@ -167,17 +136,17 @@ public class VoteCommand extends ListenerAdapter{
 								        int coins = coinsR.getInt("coins");
 								        
 								        if(unix >= timer12) {
-								        	ch.sendMessageEmbeds(new EmbedBuilder()
+								        	event.replyEmbeds(new EmbedBuilder()
 									        		.setTitle("Your Votes")
 									        		.setDescription("You have **" + coins + "** Vote-Coins \n You voted **" + votes + "** times for us!\n You can check your vote again!")
 									        		
-									        		.build()).queue();
+									        		.build()).setEphemeral(true).queue();
 										  }else
-											 ch.sendMessageEmbeds(new EmbedBuilder()
+											event.replyEmbeds(new EmbedBuilder()
 										        		.setTitle("Your Votes")
 										        		.setDescription("You have **" + coins + "** Vote-Coins \n You voted **" + votes + "** times for us!\n You can check again at** " + formattedDate + "** (MEZ)")
 										        		
-										        		.build()).queue();
+										        		.build()).setEphemeral(true).queue();
 									}catch(Exception e1) {
 										e1.printStackTrace();
 										
@@ -185,17 +154,12 @@ public class VoteCommand extends ListenerAdapter{
 								    
 					} catch (SQLException e) {
 						// TODO Auto-generated catch block
-					
-						//e.printStackTrace();
+						e.printStackTrace();
 					}
-				}else
-					Error.sendError("Wrong Input", ch, lan);
-				
-				
-				//DONT CHANGE
-				premium = false;
-				lan = "en";
-	        }
+				}
+			}
+		}
+		
 	}
 
 }
